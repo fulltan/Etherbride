a7328ed182b8255926a0f05b2ba9fec0d9c9e4e8
TakuyaFuruta <fulltan.sol@gmail.com>
2018-08-11 19:58:18 +0900

    不要ディレクトリの削除

--- etherbride/migrations/2_deploy_comtracts.js
+++
@@ -1,6 +0,0 @@
-var EngageRingToken = artifacts.require("./EngageRingToken.sol");
-
-module.exports = function(deployer) {
-    let initialSupply = 10000000;
-    deployer.deploy(EngageRingToken, initialSupply);
-};
